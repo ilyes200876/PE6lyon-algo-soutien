96596bda5de4856c2aea82acfc489489e437a523
ilyes200876 <ilyes200876@live.fr>
2023-03-03 13:46:03 +0100

    commit 3

--- ex3.php
+++ ex3.php
@@ -34,10 +34,49 @@ function afficherÉtoilesMultiligne($nb_étoiles)
   
 }
 
+/* Écrivez la routine afficherÉtoilesMultilignesRTL( var nb_étoiles).
+Elle est identique à la précédente, à l'exception que la pointe haute du triangle doit être alignée à droite
+Exemple : afficherÉtoilesMultilignesRTL(3) => 
+  *
+ **
+*** 
+*/
 
+function afficherEspaceEtoile($nb_étoiles)
+{
+  $i = 0 ;
+  $k = "" ;
+  while ($i <$nb_étoiles - 1) {
+    $k = $k . " " ;
+    $i = $i + 1 ;
+  }
+  return $k ;
+}
+
+function afficherÉtoilesMultilignesRTL($nb_étoiles)
+{ $k = "" ;
+  $result = " " ;
+  $compteur = 1 ;
+  $p = " " ;
+  while ($compteur <= $nb_étoiles) {
+    $k = afficherEspaceEtoile($nb_étoiles) ;
+    $p = $p . "*" ;
+    $result = $k . $result . "*"  ;
+    echo $result ;
+    echo PHP_EOL ;
+    $result = $p ; 
+    $nb_étoiles = $nb_étoiles - 1 ;
+  }  
+}
 
 afficherÉtoiles(4) ;
 echo PHP_EOL ;
 
 
 afficherÉtoilesMultiligne(4) ;
+echo PHP_EOL ;
+
+
+afficherÉtoilesMultilignesRTL(4) ;
+echo PHP_EOL ;
+
