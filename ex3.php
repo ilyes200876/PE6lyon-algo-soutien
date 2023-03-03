a3221bbe35b8bcea8552047416b93f6434811c3f
ilyes200876 <ilyes200876@live.fr>
2023-03-03 12:13:09 +0100

    commit 2

--- ex3.php
+++ ex3.php
@@ -3,6 +3,13 @@
 /* Écrivez la routine afficherÉtoiles( var bn_étoiles) qui reçoit en paramètre un entier positif et affiche à l'écran un nombre d'étoiles identiques à cette valeur.
 Exemple : afficherÉtoiles(3) => ***
  */
+/* Écrivez la routine afficherÉtoilesMultiligne(var nb_étoiles) qui reçoit en paramètre un entier positif, et affiche un triangle d'étoiles dont le nombre de lignes est identique à cette valeur.
+Exemple : afficherÉtoilesMultilignes(3) =>
+*
+**
+***  
+
+*/
 
  function  afficherÉtoiles($bn_étoiles)
 { 
@@ -14,5 +21,23 @@ Exemple : afficherÉtoiles(3) => ***
   }
   echo $result ;
 }
+function afficherÉtoilesMultiligne($nb_étoiles)
+{
+  $result = " " ;
+  $compteur = 1 ;
+  while ($compteur <= $nb_étoiles) {
+    $result = $result . "*" ;
+    echo $result ;
+    echo PHP_EOL ;
+    $compteur = $compteur + 1 ;
+  }
+  
+}
+
+
 
 afficherÉtoiles(4) ;
+echo PHP_EOL ;
+
+
+afficherÉtoilesMultiligne(4) ;
